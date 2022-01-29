964d0637739efd10941b0e031bbde28b7bb4029d
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-28 20:15:16 -0600

    squares toggle when clicked

--- screens/Gameplay.js
+++ screens/Gameplay.js
@@ -13,10 +13,13 @@ import { useNavigation } from "@react-navigation/native";
 const Gameplay = () => {
   const navigation = useNavigation();
   const [board, setBoard] = useState([
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
+    ["0,0", "0,1", "0,2"],
+    ["1,0", "1,1", "1,2"],
+    ["2,0", "2,1", "2,2"],
   ]);
+
+  const [xturn, setXturn] = useState(true);
+
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -30,8 +33,12 @@ const Gameplay = () => {
               {
                 row.map((square, s) => {
                   return (
-                    <TouchableOpacity style={styles.square} key={s}>
-                      <Text style={styles.buttonText}>YO</Text>
+                    <TouchableOpacity
+                      style={styles.square}
+                      key={s}
+                      onPress={() => handleTurn(r, s)}
+                    >
+                      <Text style={styles.buttonText}>{board[r][s]}</Text>
                     </TouchableOpacity>
                   ); // end inner return
                 }) // end innder map args
@@ -50,6 +57,14 @@ const Gameplay = () => {
       </View>
     </View>
   );
+
+  function handleTurn(row, square) {
+    console.log(row, square);
+    setBoard([...board], (board[row][square] = xturn ? "X" : "O"));
+
+    setXturn(!xturn);
+    // console.log(xturn);
+  }
 };
 
 export default Gameplay;
@@ -79,6 +94,8 @@ const styles = StyleSheet.create({
     backgroundColor: "gray",
     margin: 20,
     padding: 20,
+    width: 75,
+    justifyContent: "center",
   },
   row: {
     flexDirection: "row",
